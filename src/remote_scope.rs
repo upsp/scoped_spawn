82c25160fef4634f2c60db5478d8a9e8df24fbbb
upsp <upsp@protonmail.ch>
2020-06-23 05:31:11 +0000

    License under MIT License

--- src/remote_scope.rs
+++ src/remote_scope.rs
@@ -1,10 +1,3 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * This Source Code Form is "Incompatible With Secondary Licenses", as
- * defined by the Mozilla Public License, v. 2.0. */
-
 //! Low-level structured concurrency.
 //!
 //! This module gives you everything you need to spawn a task but does not do the actual spawning.
--- src/remote_scope/signals.rs
+++ src/remote_scope/signals.rs
@@ -1,10 +1,3 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * This Source Code Form is "Incompatible With Secondary Licenses", as
- * defined by the Mozilla Public License, v. 2.0. */
-
 //! Signals used by this library.
 //!
 //! See the documentation for `SignalSender` and `SignalReceiver`.
